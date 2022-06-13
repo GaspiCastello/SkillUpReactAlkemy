3fe343769f8178846bcd548c4dbca9afa1bc8759
Gaspar <gasparcastello@gmail.com>
2022-06-12 22:31:19 -0300

    favourites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,18 +1,20 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const Favorites = props => {
   const favsItems = useSelector(state => state.favs.items);
-  useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        if (!res || res.success == false) {
-          // console.log('no res in fetch');
-          return;
-        }
-        setDetails(res);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       if (!res || res.success == false) {
+  //         // console.log('no res in fetch');
+  //         return;
+  //       }
+  //       setDetails(res);
+  //     });
+  // }, []);
   return (
     <Box>
       <Heading fontSize="5xl">
@@ -27,7 +29,7 @@ const Favorites = props => {
         </Flex>
       </Heading>
 
-      <List results={results} />
+      {/* <List results={results} /> */}
     </Box>
   );
 };
